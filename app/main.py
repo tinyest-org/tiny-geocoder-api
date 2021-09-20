3073272c23c70ded99b4c6cae0eac1e04ad327a5
Paul Leveau <plawn.yay@gmail.com>
2021-09-20 21:34:29 +0200

    with cors handling

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
+import os
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .dto import GeocodeResponse
 from .resources import geocoder
 
 app = FastAPI()
 
+origins = os.environ.get('origins', '').split(',')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    # allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/geocode', response_model=GeocodeResponse)
 def geocode_path(q: str):
